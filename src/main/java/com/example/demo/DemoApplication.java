35d4141ba65b61282eb91c22e645e45fdee853fc
orange <982654045@qq.com>
2023-10-19 10:04:24 +0800

    dev2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    private String dev= "dev programmer";
+    private String dev= "dev programmer2";
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }

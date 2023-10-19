600cc74d746b48111b0945a7c199731980562a4b
orange <982654045@qq.com>
2023-10-19 09:54:09 +0800

    master patch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    private String patch = "master_patch";
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
